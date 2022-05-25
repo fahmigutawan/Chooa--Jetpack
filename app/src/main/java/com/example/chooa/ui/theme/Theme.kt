2f218951117c53bdbc68bb6c5d07f890e858976f
fahmirumagutawan <fahmigutawan@gmail.com>
2022-05-25 10:15:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/chooa/ui/theme/Theme.kt
@@ -0,0 +1,36 @@
+package com.example.chooa.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+@Composable
+fun ChooaTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    //this condition below make the theme always dark. And thats what i want h3h3
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        DarkColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
